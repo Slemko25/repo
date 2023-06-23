7cac4b512aa59c890e6ff59e772c83915818fef1
Yurii Hura <yuriihura25@gmail.com>
2023-06-23 12:57:01 +0300

    new line

--- gamelearn.py
+++ gamelearn.py
@@ -1,7 +1,7 @@
 import random
 
 y = 0
-x = 5
+x = int(input('Enter your number in a range between 0 to 11:\n'))
 def fun(x):
     for i in range(1):
         x = random.randint(0,11)
@@ -13,8 +13,8 @@ def fun(x):
             print('x is greater than y')
         else:
             print('x and y are equal')
-fun(x)
 
 while y != x :
     y = y +1
+    print(y)
     fun(x)
