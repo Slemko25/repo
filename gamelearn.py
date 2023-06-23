e8e5f78af440676c075bc6ba2b90d400459f5dd3
Yurii Hura <yuriihura25@gmail.com>
2023-06-23 12:14:10 +0300

    change in concept

--- gamelearn.py
+++ gamelearn.py
@@ -1,12 +1,20 @@
 import random
 
 y = 0
-for i in range(10):
-    x = random.random()
+x = 5
+def fun(x):
+    for i in range(1):
+        x = random.randint(0,11)
+        print(x)
 
-if x < y:
-    print('x is less than y')
-elif x > y:
-    print('x is greater than y')
-else:
-    print('x and y are equal')
+        if x < y:
+            print('x is less than y')
+        elif x > y:
+            print('x is greater than y')
+        else:
+            print('x and y are equal')
+fun(x)
+
+while y != x :
+    y = y +1
+    fun(x)
